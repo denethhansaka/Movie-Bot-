2e339bbb7446817ff0555609b9914420b4fdaefd
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-11-07 22:09:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || '2zwBBZRY#iilEYqCXhmOB8rVEzfM2fY9Fqb7z1BFerCSsr2V4-Qs', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/',// Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '94761864425',    // Enter Your Owner Number
 };
