1adb993caf3ebcb9a415034c5afd4125d2d7a0bd
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-11-07 21:58:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
+};
